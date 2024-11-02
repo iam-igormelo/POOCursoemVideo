47a21a9e45c2dd3f579bf74c0ceaf54a62fffa93
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2024-11-02 11:49:12 -0300

    Aula14 - Projeto Final

---
+++ aula14_projetoFinal/src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected char sexo;
+    protected float experiencia;
+
+    protected void ganharExp() {
+        
+    }
+}
---
+++ aula14_projetoFinal/src/Video.java
@@ -0,0 +1,22 @@
+public class Video implements AcoesVideo {
+    private String titulo;
+    private String avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+
+    @Override
+    public void play() {
+        
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void like() {
+        
+    }
+}
