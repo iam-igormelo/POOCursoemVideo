d631ad0840646959aecdb94367fd60312d59dea8
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2024-11-02 06:46:14 -0300

    Aula12 - Polimorfismo

--- aula12_polimorfismo/src/App.java
+++ aula12_polimorfismo/src/App.java
@@ -1,15 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Mamifero m = new Mamifero();
-        Reptil r = new Reptil();
-        Peixe p = new Peixe();
-        Ave a = new Ave();
+        //Reptil r = new Reptil();
+        //Peixe p = new Peixe();
+        //Ave a = new Ave();
         Canguru c = new Canguru();
         Cachorro k = new Cachorro();
-        Cobra j = new Cobra();
-        Tartaruga t = new Tartaruga();
-        GoldFish g = new GoldFish();
-        Arara e = new Arara();
+        //Cobra j = new Cobra();
+        //Tartaruga t = new Tartaruga();
+        //GoldFish g = new GoldFish();
+        //Arara e = new Arara();
 
         m.setPeso(35.3f);
         m.setCorPelo("Marrom");
