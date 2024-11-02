341b5d54312ef125c4fed2a26ce835389b081b31
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2024-11-01 22:49:48 -0300

    Aula12 - Polimorfismo

---
+++ aula12_polimorfismo/src/Canguru.java
@@ -0,0 +1,10 @@
+public class Canguru extends Mamifero {
+    @Override
+    public void locomover() {
+        System.out.println("Saltando");
+    }
+
+    public void usaBolsa() {
+        System.out.println("Usando Bolsa");
+    }
+}
