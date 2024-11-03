c6800fdb6430fc85e4fe88d3ab65203066a7c0e6
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2024-11-03 19:40:36 -0300

    Aula14 - Projeto Final

--- aula14_projetoFinal/src/Gafanhoto.java
+++ aula14_projetoFinal/src/Gafanhoto.java
@@ -2,7 +2,34 @@ public class Gafanhoto extends Pessoa {
     private String login;
     private int totAssistido;
 
+    public Gafanhoto(String nome, int idade, char sexo, String login) {
+        super(nome, idade, sexo);
+        this.login = login;
+        this.totAssistido = 0;
+    }
+
     public void viMaisUm() {
-        
+
+    }
+
+    @Override
+    public String toString() {
+        return "Gafanhoto [" + super.toString() + "login=" + login + ", totAssistido=" + totAssistido + "]";
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
     }
 }
